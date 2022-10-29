c4e230ec9a5758beba02b714e2781e8240abb99f
Jhonsonwannaa <114837630+Jhonsonwannaa@users.noreply.github.com>
2022-10-29 17:13:09 +0100

    Create dorker.py

---
+++ dorker.py
@@ -0,0 +1,30 @@
+from googlesearch import search
+import sys
+
+from pyfiglet import Figlet
+
+class couleur:
+		  	OK = '\033[91m' #GREEN
+		  	
+		  		
+
+figlet = Figlet(font='slant')
+result = figlet.renderText("Ys jhonson")
+dak= figlet.renderText("Le wana")
+print(couleur.OK+result)
+print(couleur.OK+dak)
+try :
+	a = sys.argv[1]
+	query = a
+	for dork in search(query, tld="co.in", num=100, stop=1000, pause=2):
+		  		if '=' in dork :
+		  			class couleur:
+		  				OK = '\033[92m' #GREE
+		  				print(couleur.OK+dork)
+
+
+except :
+	class couleur:
+		OK = '\033[92m' #GREEN
+	
+	print(couleur.OK+'python dorker.py target')
